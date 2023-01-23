a42a9ff163b6c7148a6be191281a54360dbd5cb6
Maksim Zakharov <yamyamyami97@gmail.com>
2023-01-23 14:49:06 +0300

    fix charts styles

--- src/components/bar_chart/BarChart.jsx
+++ src/components/bar_chart/BarChart.jsx
@@ -1,19 +1,12 @@
-import { Bar } from 'react-chartjs-2';
-import styles from './bar_chart.module.scss'
-import { Chart as ChartJS } from 'chart.js/auto'
+import { Bar } from "react-chartjs-2";
+import styles from "./bar_chart.module.scss";
+import { Chart as ChartJS } from "chart.js/auto";
 
 export default function BarChart({ barChartConfig }) {
-
-    return (
-        <div className={styles.bar_wrapper}>
-            <h1>
-                Распределение по полу и возрастам:
-            </h1>
-            <Bar data={barChartConfig} />
-        </div>
-    )
+  return (
+    <div className={styles.bar_wrapper}>
+      <h1>Распределение по полу и возрастам:</h1>
+        <Bar data={barChartConfig} />
+    </div>
+  );
 }
-
-
-
-
--- src/components/pie_chart/PieChart.jsx
+++ src/components/pie_chart/PieChart.jsx
@@ -1,19 +1,14 @@
-import { Pie } from 'react-chartjs-2';
-import styles from './pie_chart.module.scss'
-import { Chart as ChartJS } from 'chart.js/auto'
+import { Pie } from "react-chartjs-2";
+import styles from "./pie_chart.module.scss";
+import { Chart as ChartJS } from "chart.js/auto";
 
 export default function PieChart({ pieChartData }) {
-
-    return (
-        <div className={styles.pie_wrapper}>
-            <h1>
-                Распределение по сказочным животным:
-            </h1>
-            <Pie data={pieChartData} />
-        </div>
-    )
+  return (
+    <div className={styles.pie_wrapper}>
+      <h1>Распределение по сказочным животным:</h1>
+      <div>
+        <Pie data={pieChartData} />
+      </div>
+    </div>
+  );
 }
-
-
-
-
